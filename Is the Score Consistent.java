8edf2a8b12056f3e3735efba3c4a0490097887f8
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-09-09 09:27:24 +0530

    Is the Score Consistent.java

---
+++ Is the Score Consistent.java
@@ -0,0 +1,31 @@
+# Codechef-
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=0;i<T;i++)
+		{
+		        int A=sc.nextInt();
+		        int B=sc.nextInt();
+		        int C=sc.nextInt();
+		        int D=sc.nextInt();
+		        if(A<=C && B<=D)
+		        {
+		                System.out.println("POSSIBLE");
+		        }
+		        else{
+		                System.out.println("IMPOSSIBLE");
+		        }
+		}
+	}
+}
