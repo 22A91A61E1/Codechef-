24c6bfaf70544e3bd6f74ea12c5977d4a3aaa432
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-09-11 14:17:31 +0530

    Chef Score.java

---
+++ Chef Score.java
@@ -0,0 +1,31 @@
+# Codechef-
+  /* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=0;i<T;i++)
+		{
+		        int N=sc.nextInt();
+		        int X=sc.nextInt();
+		        int Y=sc.nextInt();
+		        if (Y%X==0)
+		        {
+		                System.out.println("YES");
+		        }
+		        else{
+		                System.out.println("NO");
+		        }
+		}
+	}
+}
+
