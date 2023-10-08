bf68b6e3cb1d5b37830dba1809ee56cf200fd331
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-10-08 16:47:22 +0530

    Create Buy Ten Packets.java

---
+++ Buy Ten Packets.java
@@ -0,0 +1,23 @@
+# Codechef-
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=0;i<T;i++)
+		{
+		        int x=sc.nextInt();
+		        int y=sc.nextInt();
+		        System.out.println((2*y)+x);
+		}
+	}
+}
