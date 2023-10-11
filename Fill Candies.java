6bd89b05b1d5cba53f45554e9b2d8ce9dde5bfa3
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-10-11 07:52:59 +0530

    Fill Candies.java

---
+++ Fill Candies.java
@@ -0,0 +1,30 @@
+# Codechef-
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=0;i<T;i++)
+		{
+		        int N=sc.nextInt();
+		        int K=sc.nextInt();
+		        int M=sc.nextInt();
+		        if(N%(K*M)==0)
+		        {
+		                System.out.println(N/(K*M));
+		        }
+		        else{
+		                System.out.println(N/(K*M)+1);
+		        }
+		}
+	}
+}
