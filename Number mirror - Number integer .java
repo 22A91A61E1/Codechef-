e37b57215244eb7dc42ea3203e836b7f096c98e3
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-10-12 14:05:32 +0530

    Number mirror - Number integer .java

---
+++ Number mirror - Number integer .java
@@ -0,0 +1,21 @@
+# Codechef-
+// Update the '_' in the code below
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        Scanner read = new Scanner(System.in);
+        
+        
+        int t = read.nextInt();
+        for(int i=0; i<t; i++)
+        {
+           
+            int n = read.nextInt();
+          
+            System.out.println(-n);
+        }
+	}
+}
