ab1b898d4f6a7124749536aa79a0562adece7655
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-10-12 14:02:47 +0530

    Difference between Run and  Submit.java

---
+++ Difference between Run and  Submit.java
@@ -0,0 +1,20 @@
+# Codechef-
+// Change the 'custom inputs' below and click 'run'
+// Once you have run the code - click on 'submit' to find the result with the private test files
+
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        Scanner read = new Scanner(System.in);
+        
+        int t = read.nextInt();
+        for(int i=0; i<t; i++)
+        {
+            int n = read.nextInt();
+            System.out.println(n+1);
+        }
+	}
+}
