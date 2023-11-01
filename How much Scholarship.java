8892f54aa785c9e14efc1fc1bf29737f001942d0
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-11-01 22:41:49 +0530

    How much Scholarship.java

---
+++ How much Scholarship.java
@@ -0,0 +1,28 @@
+# Codechef-
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		        Scanner sc=new Scanner(System.in);
+		        int e=sc.nextInt();
+		        if(e<=50)
+		        {
+		                System.out.println("100");
+		        }
+		        else if(e>50 && e<=100)
+		        {
+		                System.out.println("50");
+		        }
+		        else{
+		                System.out.println("0");
+		        }
+	}
+}
