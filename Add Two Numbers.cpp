209dd6337533a1211546c341d16c4baa000408f4
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-11-12 22:14:30 +0530

    Add Two Numbers.cpp

---
+++ Add Two Numbers.cpp
@@ -0,0 +1,19 @@
+# Codechef-
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+	int a;
+	int b;
+	cin>>a;
+	cin>>b;
+	cout<<a+b<<endl;
+	}
+	return 0;
+}
+
