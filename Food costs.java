09e812bae052fa9cdace3e78e62733cbd0c71e02
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-12-29 18:10:37 +0530

    Food costs.java

---
+++ Food costs.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a==25)
+		{
+		        System.out.println("CHRISTMAS");
+		}
+		else{
+		        System.out.println("ORDINARY");
+		}
+
+	}
+}
