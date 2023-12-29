6b4c3f8a460f05fd185268a10e623f7558e6715c
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-12-29 18:02:45 +0530

    Food Costs.java

---
+++ Food Costs.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+                Scanner sc=new Scanner(System.in);
+                int X=sc.nextInt();
+                int Y=sc.nextInt();
+                System.out.println((X*6)+(Y*1));
+       
+
+	}
+}
