3582a9ce0b5d93b2025340b1dd02d44c5bf688c5
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-12-30 20:48:20 +0530

    Clear Day.java

---
+++ Clear Day.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		System.out.println(7-(X+Y));
+
+	}
+}
