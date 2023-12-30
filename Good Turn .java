312163dae720be00270c5ad08390c66937e972d3
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-12-30 20:55:06 +0530

    Good Turn .java

---
+++ Good Turn .java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+    		int x = sc.nextInt();
+    		int y = sc.nextInt();
+    	if((x+y)>6)
+    	{
+    	        System.out.println("YES");
+    	}
+    	else
+    	{
+    	        System.out.println("NO");
+    	}
+		}
+		
+	}
+}
