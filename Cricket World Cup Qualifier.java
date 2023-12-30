dabdcf19a15b4ebc08b5143bfb81aff251c25465
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-12-30 20:36:36 +0530

    Cricket World Cup Qualifier.java

---
+++ Cricket World Cup Qualifier.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a<12)
+		{
+		        System.out.println("No");
+		}
+		else{
+		        System.out.println("Yes");
+		}
+
+	}
+}
