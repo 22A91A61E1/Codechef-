cade3606b1ac1c0436e0de04e1341a12b2976fa9
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-12-30 20:44:41 +0530

    Lucky Seven.java

---
+++ Lucky Seven.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String a=sc.nextLine();
+		char c=a.charAt(6);
+		System.out.println(c);
+	
+
+	}
+}
