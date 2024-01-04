ff4b14979013f17ccadc86b452b83f177753d6cb
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2024-01-04 23:25:20 +0530

    Worth of a Video.java

---
+++ Worth of a Video.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=0;i<T;i++)
+		{
+		        int S=sc.nextInt();
+		        System.out.println(S*24*1000);
+		}
+
+	}
+}
