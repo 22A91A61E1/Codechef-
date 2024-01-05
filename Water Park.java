d1e63304f03732bbde8a78cb708d435bcd3eec30
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2024-01-05 08:57:23 +0530

    Water Park.java

---
+++ Water Park.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int W=sc.nextInt();
+		int H=sc.nextInt();
+		if(60<=W && 130>=H)
+		{
+		        System.out.println("YES");
+		}
+		else
+		{
+		        System.out.println("NO");
+		}
+
+	}
+}
