59a41a09ca928e09eb3a8c3613170f0076c96a41
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2024-01-25 22:30:03 +0530

    Yearly Phone.java

---
+++ Yearly Phone.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+	        int a=X%100;
+	        if(a<10)
+	        {
+	                System.out.println("K0"+a);
+	        }
+	        else
+	        {
+	                System.out.println("K"+a);
+	        }
+		
+
+	}
+}
