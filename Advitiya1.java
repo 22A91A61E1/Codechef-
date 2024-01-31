aaf2ff88a954422ab87d36641ba51c34e4887d40
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2024-01-31 22:20:03 +0530

    Advitiya1.java

---
+++ Advitiya1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		if(N==16 || N==17 || N==18)
+		{
+		        System.out.println("ADVITIYA");
+		}
+		else{
+		        System.out.println("WAITING FOR ADVITIYA");
+		}
+
+	}
+}
