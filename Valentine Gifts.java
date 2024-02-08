5686862e8d7451e0138f6070a92928dd823048c3
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2024-02-08 22:52:24 +0530

    Valentine Gifts.java

---
+++ Valentine Gifts.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=0;i<T;i++)
+		{
+		        int X=sc.nextInt();
+		        if(X<127)
+		        {
+		                System.out.println("NO");
+		        }
+		        else{
+		                System.out.println("YES");
+		        }
+		}
+
+	}
+}
