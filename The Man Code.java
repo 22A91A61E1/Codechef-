eb65bb4f6ff440e79660bfeea7675364dd84783b
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2024-02-15 22:54:23 +0530

    The Man Code.java

---
+++ The Man Code.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=0;i<T;i++)
+		{
+		        int N=sc.nextInt();
+		        int a=(N+1)/2;
+		        int b=(N+2)/3;
+		        System.out.print(a+" "+b);
+		        System.out.println();
+		       
+		        
+		}
+
+	}
+}
