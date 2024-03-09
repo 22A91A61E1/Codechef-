4f28b8463af0dbbe05416cd8ae283bf7885f48b9
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2024-03-09 06:41:53 +0530

    Summer Time.java

---
+++ Summer Time.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		if(X>35)
+		{
+		        System.out.println("YES");
+		        
+		 
+		}
+		else{
+		        System.out.println("NO");
+		}
+		        
+
+	}
+}
