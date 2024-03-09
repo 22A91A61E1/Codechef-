63fb105ccb807e5eddb8fa37c526c98187895759
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2024-03-09 06:58:05 +0530

    Algomaniac Finals.java

---
+++ Algomaniac Finals.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+                if(X==17)
+                {
+                        System.out.println("YAY");
+                }
+	
+	        else{
+	                System.out.println("NO");
+	        }
+	}
+}
